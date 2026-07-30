92e9f16eac3c61998dfaaa2303d1f0cc74f11087
SoloDevFailure <jcxp613@gmail.com>
2026-07-30 16:50:00 +1000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import App from './App'
 import DrawingSandbox from './drawing/DrawingSandbox'
 import './styles.css'
 
-const isDrawingSandbox = window.location.pathname.replace(/\/+$/, '').endsWith('/dev/drawing')
+const redirectedRoute = new URLSearchParams(window.location.search).get('route')
+const activeRoute = (redirectedRoute ?? window.location.pathname).replace(/\/+$/, '')
+const isDrawingSandbox = activeRoute.endsWith('/dev/drawing')
+
+if (redirectedRoute) {
+  const repositoryBase = window.location.pathname.replace(/\/+$/, '')
+  window.history.replaceState(null, '', `${repositoryBase}${redirectedRoute}`)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
